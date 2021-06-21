bd41a6840a2c069f5f6a3e36c540ecf33408b8c5
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-21 11:17:54 +0200

    Mostrar y ocultar si el campo tiene errores

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -21,6 +21,14 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
     this.setMensaje();
   }
 
+  @Input() set valido( valor: boolean ) {
+    if( valor === true ) {
+      this.htmlElement.nativeElement.classList.add('hidden');
+    } else {
+      this.htmlElement.nativeElement.classList.remove('hidden');
+    }
+  }
+
   constructor( private el: ElementRef<HTMLElement> ) {
     this.htmlElement = el;
   }
