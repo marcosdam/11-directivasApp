1e591050294bd015a2e4afc166798cc06439323b
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-21 11:26:33 +0200

    Directivas estructurales

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ErrorMsgDirective } from './directives/error-msg.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 
 
 @NgModule({
   declarations: [
-    ErrorMsgDirective
+    ErrorMsgDirective,
+    CustomIfDirective
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    CustomIfDirective,
     ErrorMsgDirective
   ]
 })
