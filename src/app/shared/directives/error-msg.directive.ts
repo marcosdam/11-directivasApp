a93f5e8f97620e739b74eaffa46a6085e2ab1aa6
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-21 10:25:13 +0200

    Directiva personalizada

---
+++ src/app/shared/directives/error-msg.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[error-msg]'
+})
+export class ErrorMsgDirective implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void{
+
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ErrorMsgDirective } from './directives/error-msg.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ErrorMsgDirective
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ErrorMsgDirective
   ]
 })
 export class SharedModule { }
