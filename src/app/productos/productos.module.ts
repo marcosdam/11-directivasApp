a6b7378343910ec677586bad411951f8df3bd201
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-21 10:17:22 +0200

    Formulario reactivo

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductosRoutingModule } from './productos-routing.module';
 import { AgregarComponent } from './pages/agregar/agregar.component';
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import { AgregarComponent } from './pages/agregar/agregar.component';
   ],
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductosModule { }
