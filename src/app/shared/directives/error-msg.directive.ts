18bb0c1c0f2ae1c459e7ef5ff984deb5e49f2e08
Marcos <marcos.ledesma.picazo@cieep.com>
2021-06-21 11:08:56 +0200

    Optimizado

--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -11,14 +11,14 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
   htmlElement: ElementRef<HTMLElement>;
 
   @Input() set color( valor: string ) {
-    this.htmlElement.nativeElement.style.color = valor;
     this._color = valor;
+    this.setColor();
   }
 
   // @Input() mensaje: string = 'Este campo es requerido';
   @Input() set mensaje( valor: string ) {
-    this.htmlElement.nativeElement.innerText = valor;
     this._mensaje = valor;
+    this.setMensaje();
   }
 
   constructor( private el: ElementRef<HTMLElement> ) {
@@ -38,20 +38,21 @@ export class ErrorMsgDirective implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-    // this.setColor();
-    // this.setMensaje();
+    this.setEstilo();
+    this.setColor();
+    this.setMensaje();
   }
 
   setEstilo(): void {
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
-  // setColor(): void {
-  //   this.htmlElement.nativeElement.style.color = this.color;
-  // }
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
 
-  // setMensaje(): void {
-  //   this.htmlElement.nativeElement.innerText = this.mensaje;
-  // }
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this._mensaje;
+  }
 
 }
